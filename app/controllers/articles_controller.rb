c93a51e0a7c1eae6c7f1171b62fb6e03d22318af
Erol Ziya <erol.ziya@gmail.com>
2014-04-17 18:26:18 +0100

    Added links to edit pages. edit uses patch

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,16 @@ class ArticlesController < ApplicationController
 	def edit
 		@article = Article.find(params[:id])
 	end
+	
+	def update
+		@article = Article.find(params[:id])
+ 
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
 
 	def show
 		@article = Article.find(params[:id])
